46eb7cb756ebcc1ed42e91cb349cd2468f3511b4
Worfa <worfa@mail.ru>
2022-06-29 11:15:09 +0500

    feat: (all services) Procfile.txt для heroku

--- broomsCabinet/gateway/src/main/java/org/electricBrooms/GatewayApp.java
+++ broomsCabinet/gateway/src/main/java/org/electricBrooms/GatewayApp.java
@@ -2,7 +2,9 @@ package org.electricBrooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
+@EnableWebFluxSecurity
 @SpringBootApplication
 public class GatewayApp {
     public static void main(String[] args){
